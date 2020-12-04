e1d07a0c57e8a7252947eff2191a65255c3c7784
Sameer Ahmed <sameersahmed@hotmail.com>
2020-12-04 19:52:50 +0000

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
-class Contact < ActiveRecord::Base
 
+class Contact < ActiveRecord::Base
+  
+  validates :Name, presence: true
+  validates :Email, presence: true
+  validates :Comments, presence: true
+  
 end
-    
