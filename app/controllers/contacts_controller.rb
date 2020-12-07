c18854a6a419b423ff6569fa69c45b1b25f5edaf
Sameer Ahmed <sameersahmed@hotmail.com>
2020-12-07 19:50:24 +0000

    Trigger the mailer from the controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,11 @@ class ContactsController < ApplicationController
     
     def create
         @contact = Contact.new(contact_params)
-        if @contact.save 
+        if @contact.save
+            name = params[:contact][:Name]
+            email = params[:contact][:Email]
+            body = params[:contact][:Comments]
+            ContactMailer.contact_email(name, email, body).deliver
             flash[:success] = "Message Sent."
             redirect_to new_contact_path
         else
