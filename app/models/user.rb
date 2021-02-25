f9077b3ff4ceb6ca02ed41b07a701c3d646a9168
Sameer Ahmed <sameersahmed@hotmail.com>
2021-02-25 03:28:08 +0000

    No real progress added, still trying to debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,16 @@ class User < ApplicationRecord
     belongs_to :plan
     #has_one :profile
     
-    attr_accessor :stripe_card_token
-    def save_with_subscription
-        if valid?
-            customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
-            self.stripe_customer_token = customer.id
-            save!
-        end
+    # Whitelist this so we can grab info from the pro form
+  attr_accessor :stripe_card_token
+
+  def save_with_subscription
+    # We need to check if they pass the devise validation
+    if valid?
+      # This is the line where stripe actually charges the customer
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
     end
+  end
 end
